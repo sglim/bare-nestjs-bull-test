3a166b43e659bfce4cdf1083a03faf5f1c62ef38
Seung-gi Lim <seung.gi.lim@gmail.com>
2025-04-02 22:10:09 +0900

    Test two processes in a processor

--- src/ranking.processor.ts
+++ src/ranking.processor.ts
@@ -2,17 +2,38 @@ import { Process, Processor } from '@nestjs/bull';
 import { Job } from 'bull';
 import { promisify } from 'util';
 
+const job1Concurrency = 1;
+const job2Concurrency = 1;
+
 // ranking-processor.ts
 @Processor('rank')
 export class RankingProcessor {
   @Process({
     name: 'Job',
-    concurrency: 5,
+    concurrency: job1Concurrency,
   })
   async transcode(job: Job<string[]>) {
-    console.log('start job. It takes 10 seconds');
+    console.log(
+      'start job1. It takes 10 seconds',
+      `Job1 concurrency: ${job1Concurrency}`,
+    );
     await promisify(setTimeout)(10000);
-    console.log('end job');
+    console.log('end job1');
+    await job.progress(100);
+    return 'Done';
+  }
+
+  @Process({
+    name: 'Job2',
+    concurrency: job2Concurrency,
+  })
+  async transcode2(job: Job<string[]>) {
+    console.log(
+      'start job2. It takes 10 seconds',
+      `Job2 concurrency: ${job2Concurrency}`,
+    );
+    await promisify(setTimeout)(10000);
+    console.log('end job2');
     await job.progress(100);
     return 'Done';
   }
